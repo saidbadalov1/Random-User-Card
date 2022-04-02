78fbd3ea2096201ed947d32a8ef47dc817ca8cc7
Said Badalov <b061312021@asoiu.edu.az>
2022-04-02 22:49:11 +0400

    first

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header"
+import UserList from "./components/UserList";
+import Index from "./index.css";
+
+function App() {
+  return (
+   
+    <>
+    <Header/>
+    <UserList/>
+    </>
+
+
+  )
+}
+
+export default App;
